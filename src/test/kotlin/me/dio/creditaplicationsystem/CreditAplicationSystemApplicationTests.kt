6e05ab2b86e38f12b468132387a36248edd18f51
Carlos <carlos_lima1912@outlook.com>
2023-05-16 10:01:58 -0300

    first commit

---
+++ src/test/kotlin/me/dio/creditaplicationsystem/CreditAplicationSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.dio.creditaplicationsystem
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CreditAplicationSystemApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
